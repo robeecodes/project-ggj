e1bcafd9bd6b530e8dfeb3481be2c145029c70df
R-Jassasi <93593202+R-Jassasi@users.noreply.github.com>
2024-01-27 14:33:25 +0000

    Adding gates

---
+++ Assets/MASH Virtual/GateTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateTrigger : MonoBehaviour
+{
+   public Animator doorAnim;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Opening Door");
+            doorAnim.ResetTrigger("close");
+            doorAnim.SetTrigger("open");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Closing Door");
+            doorAnim.ResetTrigger("open");
+            doorAnim.SetTrigger("close");
+        }
+    }
+
+
+}
